b66069a06ae968d0ad6609e070b1411b4f1f23f1
shoutatani <10176164+shoutatani@users.noreply.github.com>
2020-08-26 12:51:14 +0900

    Added react on rails

---
+++ Rails/frontend/webpack.common.js
@@ -0,0 +1,89 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    "index": "./src/javascripts/index.tsx",
+  },
+  output: {
+    path: path.resolve(__dirname, "../public/frontend/assets"),
+    publicPath: "/frontend/assets/",
+    filename: "[name].js"
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          // MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              importLoaders: 2
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+              plugins: [
+                require("autoprefixer")({
+                  grid: true
+                })
+              ]
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: {
+          loader: "file-loader",
+          options: {
+            outputPath: './images',
+          },
+        }
+      },
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    // new CopyPlugin({
+    //   patterns: [
+    //     { from: 'src/index.html', to: '../' },
+    //   ]
+    // }),
+  ]
+
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  // externals: {
+  //   react: "React",
+  //   "react-dom": "ReactDOM"
+  // }
+};
